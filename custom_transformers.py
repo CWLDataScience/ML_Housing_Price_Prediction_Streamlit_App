87e00e7686a4114835cc9da28a477f6252e715e4
CWLDataScience <chenweilongbusiness@gmail.com>
2026-05-06 23:14:36 +0800

    Create custom_transformers.py

---
+++ custom_transformers.py
@@ -0,0 +1,27 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import rbf_kernel
+
+class ClusterSimilarity(BaseEstimator, TransformerMixin):
+    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
+        self.n_clusters = n_clusters
+        self.gamma = gamma
+        self.random_state = random_state
+
+    def fit(self, X, y=None, sample_weight=None):
+        self.kmeans_ = KMeans(
+            n_clusters=self.n_clusters,
+            n_init=10,
+            random_state=self.random_state
+        )
+        self.kmeans_.fit(X, sample_weight=sample_weight)
+        return self
+
+    def transform(self, X):
+        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
+
+def compute_ratio(X):
+    return X[:, [0]] / X[:, [1]]
+
+def ratio_feature_names(transformer, feature_names_in):
+    return ["ratio"]
