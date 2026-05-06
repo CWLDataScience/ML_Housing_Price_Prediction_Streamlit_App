b46523443f076fbc45d7525190a1d7fed4d5ee46
CWLDataScience <chenweilongbusiness@gmail.com>
2026-05-06 23:16:49 +0800

    Update app.py
    created custom transformer module

--- app.py
+++ app.py
@@ -6,32 +6,7 @@ import pydeck as pdk
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import rbf_kernel
-
-# ---- Custom transformer ----
-class ClusterSimilarity(BaseEstimator, TransformerMixin):
-    def __init__(self, n_clusters=10, gamma=1.0, random_state=None):
-        self.n_clusters = n_clusters
-        self.gamma = gamma
-        self.random_state = random_state
-
-    def fit(self, X, y=None, sample_weight=None):
-        self.kmeans_ = KMeans(
-            n_clusters=self.n_clusters,
-            n_init=10,
-            random_state=self.random_state
-        )
-        self.kmeans_.fit(X, sample_weight=sample_weight)
-        return self
-
-    def transform(self, X):
-        return rbf_kernel(X, self.kmeans_.cluster_centers_, gamma=self.gamma)
-
-# ---- Custom functions ----
-def compute_ratio(X):
-    return X[:, [0]] / X[:, [1]]
-
-def ratio_feature_names(transformer, feature_names_in):
-    return ["ratio"]
+from custom_transformers import *
 
 # ---- Load model ----
 model = joblib.load("housing_model.pkl2")
